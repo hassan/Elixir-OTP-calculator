bccf0c4abaf56b04e0c70ec7729fc6a7e47bb2a2
Hassan Schroeder <hassan@webtuitive.com>
2015-09-26 15:25:41 -0700

    Get some basic tests working.

--- lib/calculation/server.ex
+++ lib/calculation/server.ex
@@ -1,32 +1,60 @@
 defmodule Calculation.Server do
   use GenServer
 
-  def handle_call({:reset, value}, _pid, current_value) do
-    { :reply, value, value }
+  def handle_call({:reset}, _pid, state) do
+    state = Map.put(state, :current, Map.get(state, :baseline))
+    { :reply, state, state }
   end
 
-  def handle_call({:add, value}, _pid, current_value) do
-    { :reply, current_value + value, current_value + value }
+  def handle_call({:reset, value}, _pid, state) do
+    state = Map.put(state, :current, value)
+    state = Map.put(state, :baseline, value)
+    { :reply, state, state }
   end
 
-  def handle_call({:subtract, value}, _pid, current_value) do
-    { :reply, current_value - value, current_value - value }
+  def handle_call({:add, value}, _pid, state) do
+    state = added(state, value)
+    { :reply, state, state }
   end
 
-  def handle_call({:multiply, value}, _pid, current_value) do
-    { :reply, current_value * value, current_value * value }
+  def handle_call({:subtract, value}, _pid, state) do
+    state = subtracted(state, value)
+    { :reply, state, state }
+  end
+
+  def handle_call({:multiply, value}, _pid, state) do
+    state = multiplied(state, value)
+    { :reply, state, state }
   end
 
   # def handle_call({:multiply, first, second}, _pid, current_value) do
   #   { :reply, first * second, first * second }
   # end
 
-  def handle_call({:divide, value}, _pid, current_value) do
-    { :reply, current_value / value, current_value / value }
+  def handle_call({:divide, value}, _pid, state) do
+    state = divided(state, value)
+    { :reply, state, state }
   end
 
-  def handle_call(:value, _pid, current_value) do
-    { :reply, current_value, current_value }
+  def handle_call(:current_value, _pid, state) do
+    # current_value = Map.get(state, :current)
+    { :reply, state, state }
   end
+
+  defp added(state, value) do
+    Map.put( state, :current, Map.get( state, :current ) + value )
+  end
+  defp subtracted(state, value) do
+    Map.put( state, :current, Map.get( state, :current ) - value )
+  end
+  defp multiplied(state, value) do
+    Map.put( state, :current, Map.get( state, :current ) * value )
+  end
+  defp divided(state, value) do
+    Map.put( state, :current, Map.get( state, :current ) / value )
+  end
+
+
+
 
 end
--- test/calculation_test.exs
+++ test/calculation_test.exs
@@ -1,8 +1,33 @@
 defmodule CalculationTest do
-  use ExUnit.Case
-  doctest Calculation
+  use ExUnit.Case, async: true
+  # doctest Calculation
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it starts" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    {:links, links} = Process.info(self, :links)
+    assert pid in links
   end
+  test "it adds" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :add, 100 }) == %{baseline: 1000, current: 1100}
+  end
+  test "it subtracts" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :subtract, 100 }) == %{baseline: 1000, current: 900}
+  end
+  test "it multiplies" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :multiply, 2 }) == %{baseline: 1000, current: 2000}
+  end
+  test "it divides" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :divide, 2 }) == %{baseline: 1000, current: 500}
+  end
+  test "it handles multiple calls" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1100}
+    assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1200}
+    assert GenServer.call(pid, { :divide, 2 }) == %{baseline: 1000, current: 600}
+  end
+
 end
