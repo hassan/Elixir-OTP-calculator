1249fc7b6045d6a375e6a12ad399b7d234e537a3
Hassan Schroeder <hassan@webtuitive.com>
2015-09-26 15:35:27 -0700

    Additional tests.

--- test/calculation_test.exs
+++ test/calculation_test.exs
@@ -1,33 +1,49 @@
 defmodule CalculationTest do
   use ExUnit.Case, async: true
-  # doctest Calculation
 
   test "it starts" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     {:links, links} = Process.info(self, :links)
     assert pid in links
   end
+
   test "it adds" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     assert GenServer.call(pid, { :add, 100 }) == %{baseline: 1000, current: 1100}
   end
+
   test "it subtracts" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     assert GenServer.call(pid, { :subtract, 100 }) == %{baseline: 1000, current: 900}
   end
+
   test "it multiplies" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     assert GenServer.call(pid, { :multiply, 2 }) == %{baseline: 1000, current: 2000}
   end
+
   test "it divides" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     assert GenServer.call(pid, { :divide, 2 }) == %{baseline: 1000, current: 500}
   end
+
   test "it handles multiple calls" do
     {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
     assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1100}
     assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1200}
     assert GenServer.call(pid, { :divide, 2 }) == %{baseline: 1000, current: 600}
+  end
+
+  test "it resets to the baseline value" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1100}
+    assert GenServer.call(pid, {:reset}) == %{baseline: 1000, current: 1000}
+  end
+
+  test "it resets to a new baseline value" do
+    {:ok, pid} = GenServer.start_link(Calculation.Server, %{baseline: 1000, current: 1000})
+    assert GenServer.call(pid, { :add, 100 })  == %{baseline: 1000, current: 1100}
+    assert GenServer.call(pid, {:reset, 2000}) == %{baseline: 2000, current: 2000}
   end
 
 end
